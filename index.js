26c04e7d215a89252b398205773b0a35b9c928b1
luluwaffless <lcsgoes210@gmail.com>
2025-05-31 17:45:51 -0300

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
-const app = express();
-app.get("*", (req, res) => res.redirect(`roblox:/${req.path}?${new URLSearchParams(req.query).toString()}`));
-app.listen(process.env.PORT || 80);
+express()
+    .use(express.static("public"))
+    .get("*", (req, res) => res.redirect(`roblox:/${req.path}?${new URLSearchParams(req.query).toString()}`))
+    .listen(process.env.PORT || 80);
