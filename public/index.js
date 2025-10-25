15d2decfb2b99335b5adac6a670ea5fa781aff26
luluwaffless <lcsgoes210@gmail.com>
2025-10-25 17:25:30 -0300

    static js change

--- public/index.js
+++ public/index.js
@@ -1,8 +1,12 @@
 const copy = document.getElementById('copy');
 const openBtn = document.getElementById('open');
-const url = document.getElementById('url');
+const urlEl = document.getElementById('url');
 const params = {};
-const updateUrl = () => url.innerHTML = `${window.location.origin}/experiences/start${Object.keys(params).length ? `?${new URLSearchParams(params).toString()}` : ""}`;
+let url = `${window.location.origin}/experiences/start`;
+const updateUrl = () => {
+    url = `${window.location.origin}/experiences/start${Object.keys(params).length ? `?${new URLSearchParams(params).toString()}` : ""}`
+    urlEl.innerHTML = url;
+};
 ['placeId', 'gameInstanceId', 'userId', 'accessCode', 'linkCode', 'launchData'].forEach(param => {
     const element = document.getElementById(param);
     element.addEventListener('input', () => {
@@ -17,4 +21,4 @@ copy.addEventListener('click', () => {
         setTimeout(() => copy.innerHTML = 'copy URL', 2000);
     });
 });
-openBtn.addEventListener('click', () => window.location.href = url.innerHTML);
+openBtn.addEventListener('click', () => window.location.href = url);
